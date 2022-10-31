c2e7e71e3b66bb44480e835ee22742e4e0706688
bearom95 <bearom95@gmail.com>
2022-10-31 12:20:40 +0100

    animals React listo con server

---
+++ README.md
@@ -0,0 +1,2 @@
+# boiler
+boilerplate para proyectos
---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>all good</h1>
+    </div>
+  );
+}
+
+export default App;
